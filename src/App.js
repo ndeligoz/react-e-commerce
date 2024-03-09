98efbd98ca7ab401f5e847b744428209170e3bf8
ndeligoz <nndeligoz@gmail.com>
2024-03-10 01:14:43 +0300

    feat: created e-commerce appjs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-// App.js
-
 import React, { useState, useEffect } from "react";
+import { Route, Routes } from "react-router-dom";
 import CategoryFilter from "./components/CategoryFilter/CategoryFilter";
 import ProductList from "./components/ProductList/ProductList";
-import Empty from "./components/Empty/Empty"; // Yeni eklenen component
 import Pagination from "./components/Pagination/Pagination";
+import ProductDetail from "./components/ProductDetail/ProductDetail";
 import "./App.css";
 
 const App = () => {
   const [products, setProducts] = useState([]);
   const [categories, setCategories] = useState([]);
-  const [selectedCategory, setSelectedCategory] = useState("");
+  const [selectedCategories, setSelectedCategories] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
   const [currentPage, setCurrentPage] = useState(1);
   const [productsPerPage] = useState(12);
+  const [isListView, setIsListView] = useState(true);
 
+  console.log(selectedCategories);
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await fetch("https://fakestoreapi.com/products");
         const data = await response.json();
+        console.log("data", data);
 
         const starredProducts =
           JSON.parse(localStorage.getItem("starredProducts")) || {};
@@ -41,13 +43,15 @@ const App = () => {
   }, []);
 
   const handleCategoryChange = (event, category) => {
-    console.log("Seçilen Kategori:", category);
-
-    // Eğer kategori tekrar seçiliyse, kategoriyi kaldır
-    setSelectedCategory((prevCategory) =>
-      prevCategory === category ? null : category
-    );
-
+    setSelectedCategories((prevCategories) => {
+      // Eğer seçilen kategori zaten mevcut ise, onu listeden çıkar.
+      if (prevCategories.includes(category)) {
+        return prevCategories.filter((c) => c !== category);
+      } else {
+        // Mevcut kategorilere yeni kategoriyi ekle.
+        return [...prevCategories, category];
+      }
+    });
     setCurrentPage(1);
   };
 
@@ -76,11 +80,14 @@ const App = () => {
         }, {})
       )
     );
-  }, [selectedCategory, products]);
+  }, [selectedCategories, products]);
 
-  const filteredProducts = selectedCategory
-    ? products.filter((product) => product.category === selectedCategory)
-    : products;
+  const filteredProducts =
+    selectedCategories.length > 0
+      ? products.filter((product) =>
+          selectedCategories.includes(product.category)
+        )
+      : products;
 
   const searchedProducts = searchTerm
     ? filteredProducts.filter((product) =>
@@ -97,24 +104,44 @@ const App = () => {
 
   const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
+  const handleViewChange = (isList) => {
+    setIsListView(isList);
+  };
+
   return (
-    <div>
-      <h1>Ürünler</h1>
-      <CategoryFilter
-        categories={categories}
-        onCategoryChange={handleCategoryChange}
-        onSearchChange={handleSearchChange}
-      />
-      {currentProducts.length === 0 ? (
-        <Empty message="Ürün bulunamadı." />
-      ) : (
-        <ProductList products={currentProducts} onStarClick={handleStarClick} />
-      )}
-      <Pagination
-        currentPage={currentPage}
-        totalPages={Math.ceil(searchedProducts.length / productsPerPage)}
-        onPageChange={paginate}
-      />
+    <div className="layout">
+      <div className="categories">
+        <CategoryFilter
+          categories={categories}
+          onCategoryChange={handleCategoryChange}
+          onSearchChange={handleSearchChange}
+        />
+      </div>
+      <div className="sales">
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <ProductList
+                products={currentProducts}
+                onStarClick={handleStarClick}
+                isListView={isListView}
+                handleViewChange={handleViewChange}
+              />
+            }
+          />
+          <Route path="/product/:productId" element={<ProductDetail />} />
+        </Routes>
+        <div className="pagination">
+          {currentProducts.length > 0 && (
+            <Pagination
+              currentPage={currentPage}
+              totalPages={Math.ceil(searchedProducts.length / productsPerPage)}
+              onPageChange={paginate}
+            />
+          )}
+        </div>
+      </div>
     </div>
   );
 };
